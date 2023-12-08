9eada58baae05d358a40040a404d4fd4940acb73
Eugene Bravov <eugene.bravov@gmail.com>
2023-12-08 12:21:43 -0300

    up

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,11 @@
-FROM webdevops/php-nginx:7.3
+FROM webdevops/php-nginx:8.2
 
 ENV WEB_DOCUMENT_ROOT /app/public
 WORKDIR /app
+
 COPY composer.json composer.json
 COPY composer.lock composer.lock
 
-RUN composer install
+RUN composer install --no-scripts
 
 COPY . .
--- config/bundles.php
+++ config/bundles.php
@@ -2,6 +2,5 @@
 
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
-    Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
-    Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
+    Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
 ];
